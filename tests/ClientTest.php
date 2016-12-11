a8d710f9740ab889c59f4e2b4f4429c6303e9d94
Judas Borbon <dexter.x740@gmail.com>
2016-12-11 17:17:09 -0600

    Add tests for parsing options

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -14,11 +14,11 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $client = new Client();
     }
 
-    public function test_it_parses_options()
+    public function test_it_overrides_provided_options()
     {
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $client = new Client('APIKEY', ['ssl' => true, 'log' => true]);
+        $this->assertEquals($client->getOptionValue('ssl'), true);
+        $this->assertEquals($client->getOptionValue('log'), true);
     }
 
     public function test_throws_error_when_trying_to_get_invalid_option()
@@ -26,6 +26,14 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $this->expectException(ClockworkSmsException::class);
 
         $client = new Client('APIKEY');
+        $client->getOptionValue('foo');
+    }
+
+    public function test_ignores_invalid_options_provided()
+    {
+        $this->expectException(ClockworkSmsException::class);
+
+        $client = new Client('APIKEY', ['log' => true, 'foo' => 'bar']);
         $client->getOptionValue('foo');
     }
 }
